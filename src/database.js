867439214cff6b2be664010076ea586b55e436c5
alexandro157 <alexandro.choque@tecsup.edu.pe>
2023-09-26 02:18:03 -0500

    first commit

---
+++ src/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/simplejwt',{
+    
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+})
+
+
+    .then(db => console.log('La base de datos está conectada.')); 
